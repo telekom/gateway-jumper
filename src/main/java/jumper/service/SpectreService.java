e29a9aea2861e1a95ad674683190aeb36171d816
Christian Ingenhaag <christian.ingenhaag@telekom.de>
2025-09-23 15:14:58 +0200

    chore: proxyConfig info log + explicit webclient bean qualifier usage

--- src/main/java/jumper/service/SpectreService.java
+++ src/main/java/jumper/service/SpectreService.java
@@ -22,6 +22,7 @@ import jumper.model.config.SpectreKind;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
@@ -43,6 +44,8 @@ public class SpectreService {
 
   private final OauthTokenUtil oauthTokenUtil;
   private final Tracer tracer;
+
+  @Qualifier("spectreServiceWebClient")
   private final WebClient spectreServiceWebClient;
 
   @Value("${jumper.stargate.url}")
